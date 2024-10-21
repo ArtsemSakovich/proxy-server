bf77ea4c1b0da25c173b04059186c0dcc036e9c4
a.sakovich <artsem.sakovich@godel.shellenergy.co.uk>
2024-10-21 17:31:00 +0200

    Proxy Server Lab - Task #2

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const axios = require("axios");
 
-console.log('hello world!');
+axios
+    .get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-10-14&end_date=2024-10-18&api_key=II1sjzN43X13j2PXI3Cx4sQBfHQT5cc5B8qKGr2V")
+    .then((response) => {
+        console.log("Data received:", response.data);
+    })
+    .catch((error) => {
+        if (error.response) {
+            console.error(`HTTP error: ${error.response.status}`);
+        } else if (error.request) {
+            console.error("Request error: No response received");
+        } else {
+            console.error("Error:", error.message);
+        }
+    });
+
